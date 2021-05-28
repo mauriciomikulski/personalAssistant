2d9f58db0cdb7315ac1d65fdfffc1e450568f14c
Mauricio Mikulski <incognitomikulski@gmail.com>
2021-05-28 15:00:19 -0300

    add wake_on mod

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import datetime
+from initComputers import wake_on_lan
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
-
+global command
 
 def talk(text):
   engine.say(text)
   engine.runAndWait()
 
-def take_command():
+def take_command():  
   try:
-    with sr.Microphone() as source:
+    with sr.Microphone() as source:   
       listener.adjust_for_ambient_noise(source)
       print('listening...')
       voice = listener.listen(source)
@@ -39,6 +40,9 @@ def run_alexa():
   elif 'horas' in command:
     time = datetime.datetime.now().strftime('%H:%M')
     talk('Agora são exatamente ' + time)
+  elif 'ligar pc' in command:
+    talk('Ligando computador')
+    wake_on_lan('0A-E0-AF-D2-20-23')
 
 
 run_alexa()
